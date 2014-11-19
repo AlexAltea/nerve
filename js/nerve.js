9947fcc485c72154c70d59709e41793d25e71eba
Alexandro Sánchez Bach <alexaltea123@gmail.com>
2014-11-19 13:15:30 +0100

    List of CPU threads and documentation fixes

--- js/nerve.js
+++ js/nerve.js
@@ -1,81 +1,104 @@
 /**
  * Global Nerve object
  */
-Nerve = {};
+Nerve = {
+    // IP:Port of the back-end server
+    server: undefined,
 
-/**
- * Connect to the debugger back-end.
- * Since the back-end is a RESTful API, no real permanent connection is established, the only
- * putpose of this function is validating the user's server address and updating the UI.
- */
-function connect(server) {
-    
-    // Get and server address
-    server = server.trim();
-    if (!(/^http/).test(server)) {
-        server = "http://" + server;
-    }
-    
-    $("#nerve-server-btn").html("<div class='loader'>Loading...</div>");
-    
-    // Connect to server
-    $.getJSON(server + "/connect", function(data) {
-        // Make tooltip
-        var serverTooltip = "<table style='text-align: left'>";
-        serverTooltip += "<tr><td style='width: 50px;'></td><td></td></tr>";
-        serverTooltip += "<tr><td>Device:</td><td>" + data.device.name + " (" + data.device.version + ")</td></tr>";
-        if (data.host) {
-            serverTooltip += "<tr><td>Host:</td><td>" + data.host.os + " (" + data.host.cpu + ")</td></tr>";
+    /**
+     * Connect to the debugger back-end.
+     * Since the back-end is a RESTful API, no real permanent connection is established, the only
+     * putpose of this function is validating the user's server address and updating the UI.
+     */
+    connect: function(server){
+        $("#nerve-server-btn").html("<div class='loader'>Loading...</div>");
+        
+        // Get and server address
+        server = server.trim();
+        if (!(/^http/).test(server)) {
+            server = "http://" + server;
         }
-        serverTooltip += "</table>"
-        
-        // Replace form with server information
-        var ipPort = server.replace("http://", "").replace("/", "");
-        $("#nerve-server-btn").html("Connect...");
-        $("#nerve-server-form").hide();
-        $("#nerve-server-status").html("Connected to: <i>" + ipPort + "</i>");
-        $("#nerve-server-status").attr('data-original-title', serverTooltip);
-        $("#nerve-server-status").tooltip({container: 'body'});
-        $("#nerve-server-status").show();
-        $("#nerve-server-actions").show();
-        
-        // Save information
-        Nerve.server = server;
-        
-    }).error(function(){
-        $("#nerve-server-btn").html("Connect...");
-        showModal({
-            title: "Connection error",
-            body: "<p>Could not connect to the specified Nerve back-end server. " +
-            "Make sure the debugger is running, and the address is correct " + 
-            "and entered in the format <b>IP:Port</b>.</p>" +
-            "<p>For example: <i>127.0.0.1:8080</i></p>",
-            footer: "<button type='button' class='btn btn-primary' data-dismiss='modal'>Close</button>"
-        });
-    });
-}
 
-/**
- * Disconnect from the debugger back-end.
- * Since the back-end is a RESTful API, no real disconnection happens here, just the UI
- * is updated to allow the user connect to a different server.
- */
-function disconnect() {
-    $("#nerve-server-status").hide();
-    $("#nerve-server-actions").hide();
-    $("#nerve-server-addr").val("");
-    $("#nerve-server-form").show();
-}
+        // Connect to server
+        $.getJSON(server + "/connect", function(data) {
+            // Make tooltip
+            var serverTooltip = "<table style='text-align: left'>";
+            serverTooltip += "<tr><td style='width: 50px;'></td><td></td></tr>";
+            serverTooltip += "<tr><td>Device:</td><td>" + data.device.name + " (" + data.device.version + ")</td></tr>";
+            if (data.host) {
+                serverTooltip += "<tr><td>Host:</td><td>" + data.host.os + " (" + data.host.cpu + ")</td></tr>";
+            }
+            serverTooltip += "</table>"
+
+            // Replace form with server information
+            var ipPort = server.replace("http://", "").replace("/", "");
+            $("#nerve-server-btn").html("Connect...");
+            $("#nerve-server-form").hide();
+            $("#nerve-server-status").html("Connected to: <i>" + ipPort + "</i>");
+            $("#nerve-server-status").attr('data-original-title', serverTooltip);
+            $("#nerve-server-status").tooltip({container: 'body'});
+            $("#nerve-server-status").show();
+            $("#nerve-server-actions").show();
+            
+            uiTabsEnableAll();
+
+            // Save information
+            Nerve.server = server;
+
+        }).error(function(){
+            $("#nerve-server-btn").html("Connect...");
+            showModal({
+                title: "Connection error",
+                body: "<p>Could not connect to the specified Nerve back-end server. " +
+                "Make sure the debugger is running, and the address is correct " + 
+                "and entered in the format <b>IP:Port</b>.</p>" +
+                "<p>For example: <i>127.0.0.1:8080</i></p>",
+                footer: "<button type='button' class='btn btn-primary' data-dismiss='modal'>Close</button>"
+            });
+        });
+    },
+    
+    /**
+     * Disconnect from the debugger back-end.
+     * Since the back-end is a RESTful API, no real disconnection happens here, just the UI
+     * is updated to allow the user connect to a different server.
+     */
+    disconnect: function() {
+        $("#nerve-server-status").hide();
+        $("#nerve-server-actions").hide();
+        $("#nerve-server-addr").val("");
+        $("#nerve-server-form").show();
+    },
+
+    /**
+     * AJAX request to the back-end server
+     */
+    request: function(method, uri, handler) {
+        $.getJSON(this.server + uri, function(data) {
+            return handler(data);
+        }).error(function(){
+            // TODO: Show error message and disconnect
+        });
+    }
+};
 
 // Entry
 $(document).ready(function() {
-    
+
     // Activate Bootstrap components
     $("[data-toggle=tooltip]").tooltip({container: 'body', html: true});
+
+    // Load tab content
+    $("#intro").load("nerve/intro.html");
+    $("#general").load("nerve/general.html");
+    $("#cpu").load("nerve/cpu.html");
+    $("#gpu").load("nerve/gpu.html");
+    $("#memory").load("nerve/memory.html");
+    $("#system").load("nerve/system.html");
     
     // Events
     $("#nerve-server-form").submit(function() {
-        connect($("#nerve-server-addr").val());
+        Nerve.connect($("#nerve-server-addr").val());
         return false;
     });
     $('.disabled').click(function() { 
@@ -83,47 +106,25 @@ $(document).ready(function() {
     });
     
     // UI
+    uiTabsDisableAll();
     initRegisters();
-    
+
     // Debugging purposes
-    //connect("127.0.0.1");
+    /*Nerve.connect("private-fd795-nerve2.apiary-mock.com");
+    uiTabsShow("#cpu");*/
+});
+
+$(document).on('shown.bs.tab', 'a[data-toggle="tab"]', function (e) {
+    uiTabsChanged($(e.target).attr('href'));
 });
 
 /**
- * Nerve
+ * Utils
  */
 function valueToHex(value, bytes) {
     return ("0000000000000000" + value.toString(16).toUpperCase()).substr(-bytes*2); 
 }
 
-var app = angular.module("Nerve", []);
-
-app.controller("cpuController", function($scope) {
-    // Disassembler
-    $scope.disassembler = {
-        addr : 0x00000000,
-        
-        lines : function () {
-            var lines = [];
-            for (var i=0; i<16; i++) {
-                lines.push({
-                    breakpoint : "",
-                    addr : valueToHex($scope.disassembler.addr + 4*i, 4),
-                    value : "????????",
-                    instruction : "????????",
-                    comment : ""
-                });
-            }
-            return lines;
-        }
-    };
-
-    
-    
-    
-    // Registers
-    $scope.registers = [];
-    for (var i=0; i<32; i++) $scope.registers.push({ "name" : "r" +i, "value" : "????????????????" });
-    for (var i=0; i<32; i++) $scope.registers.push({ "name" : "fr"+i, "value" : "????????????????" });
-    for (var i=0; i<32; i++) $scope.registers.push({ "name" : "v" +i, "value" : "???????? ???????? ???????? ????????" });
-});
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase();
+}
--- js/ui.js
+++ js/ui.js
@@ -1,5 +1,45 @@
 /**
- * Adjust Modal parameters and display it
+ * Tabs management
+ */
+
+// Disable all tabs
+function uiTabsDisableAll() {
+    $(".navbar-nav > li > a").not("#tab-intro").addClass("disabled");
+    $(".navbar-nav > li > a").not("#tab-intro").removeAttr("data-toggle");
+}
+
+// Enable all tabs
+function uiTabsEnableAll() {
+    $(".navbar-nav > li > a").not("#tab-intro").removeClass('disabled');
+    $(".navbar-nav > li > a").not("#tab-intro").attr("data-toggle", "tab")
+}
+
+// Resize containers based on the current tab
+function uiTabsChanged(currentTab) {
+    if (currentTab == "#intro") {
+        originalWidth = $(".container").data('originalWidth');
+        $(".container").animate({width: originalWidth});
+        $(".container").data('originalWidth', undefined);
+    }
+    else {
+        if (!$(".container").data('originalWidth')) {
+            originalWidth = $(".container").css('width');
+            $(".container").data('originalWidth', originalWidth);
+        }
+        $(".container").animate({width: "100%"});
+    }
+}
+
+// Change to a different tab
+function uiTabsShow(targetTab) {
+    $(".tab-pane").removeClass("active");
+    $(targetTab).addClass("active");
+    uiTabsChanged(targetTab);
+}
+
+/**
+ * Display Bootstrap Modal:
+ * It adjusts the Modal parameters and displays it
  */
 function showModal(args) {
     $(".modal-title").html(args.title);
@@ -15,21 +55,3 @@ function showModal(args) {
 function initRegisters() {
     $("#nerve-cpu-registers").css("height", "calc(100% - 60px)");
 }
-
-angular.module('buttonsRadio', [])
-       .directive('buttonsRadio', function() {
-        return {
-            restrict: 'E',
-            scope: { model: '=', options:'='},
-            controller: function($scope){
-                $scope.activate = function(option){
-                    $scope.model = option;
-                };      
-            },
-            template: "<button type='button' class='btn' "+
-                        "ng-class='{active: option == model}'"+
-                        "ng-repeat='option in options' "+
-                        "ng-click='activate(option)'>{{option}} "+
-                      "</button>"
-        };
-    });
