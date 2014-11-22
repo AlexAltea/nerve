ae25261c97c945d87b91c2af3c71f54fff3d141e
Alexandro Sánchez Bach <alexaltea123@gmail.com>
2014-11-22 18:17:26 +0100

    Transition to a Node / Bower / Grunt environment

---
+++ Gruntfile.js
@@ -0,0 +1,122 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+    // Load tasks from grunt-* dependencies in package.json
+    require('load-grunt-tasks')(grunt);
+
+    // Time how long tasks take
+    require('time-grunt')(grunt);
+
+    // Project configuration.
+    grunt.initConfig({
+        yeoman: {
+            app: 'app',
+            dist: 'dist'
+        },
+        copy: {
+            app: {
+                expand: true,
+                filter: 'isFile',
+                cwd: '<%= yeoman.app %>/',
+                src: '**/*.html',
+                dest: '<%= yeoman.dist %>/'
+            }
+        },
+        wiredep: {
+            app: {
+                src: ['<%= yeoman.dist %>/index.html']
+            }
+        },
+        typescript: {
+            base: {
+                src: ['<%= yeoman.app %>/scripts/**/*.ts'],
+                dest: '<%= yeoman.dist %>/nerve.js',
+                options: {
+                    target: 'es5'
+                }
+            }
+        },
+        cssmin: {
+            combine: {
+                files: {
+                    '<%= yeoman.dist %>/nerve.css': [
+                        '<%= yeoman.app %>/styles/**/*.css'
+                    ]
+                }
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: '<%= yeoman.dist %>',
+                    src: '**/*.html',
+                    dest: '<%= yeoman.dist %>/'
+                }]
+            }
+        },
+        connect: {
+            options: {
+                port: 9000,
+                livereload: 35728,
+                hostname: 'localhost',
+                base: '<%= yeoman.dist %>'
+            },
+            livereload: {
+                options: {
+                    open: true
+                }
+            }
+        },
+        watch: {
+            scripts: {
+                files: '<%= yeoman.app %>/scripts/{,*/}*.ts',
+                tasks: ['typescript']
+            },
+            styles: {
+                files: '<%= yeoman.app %>/styles/{,*/}*.css',
+                tasks: ['cssmin']
+            },
+            views: {
+                files: '<%= yeoman.app %>/{,*/}*.html',
+                tasks: ['copy', 'wiredep', 'htmlmin']
+            },
+            livereload: {
+                files: [
+                    '<%= yeoman.dist %>/{,*/}*.html',
+                    '<%= yeoman.dist %>/*.js',
+                    '<%= yeoman.dist %>/*.css'
+                ],
+                options: {
+                    livereload: '<%= connect.options.livereload %>'
+                }
+            },
+        }
+    });
+
+    // Project tasks
+    grunt.registerTask('test', [
+
+    ]);
+    grunt.registerTask('build', [
+        'copy',
+        'wiredep',
+        'typescript',
+        'cssmin',
+        'htmlmin'
+    ]);
+    grunt.registerTask('serve', [
+        'connect',
+        'watch'
+    ]);
+    grunt.registerTask('default', [
+        'test',
+        'build',
+        'serve'
+    ]);
+};
--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 Nerve
 =====
+[![Last Release](https://badge.fury.io/gh/AlexAltea%2Fnerve.svg)](https://github.com/AlexAltea/nerve/releases)
 
-Front-end for PlayStation 3 debuggers.
+Front-end for PlayStation 3 debuggers. Please, understand that I'm not interested on providing support, releases, or attention to any comments, issues or pull requests for now.
+
+### Deploy
+To build, clone the *master* branch of this repository, and run following commands on it:
+
+Install the development and client dependencies:
+```
+npm install
+bower install
+```
+
+Install the TypeScript definiton files from the dependencies:
+```
+tsd install angular
+tsd install jquery
+```
+
+Finally, build the app with:
+```
+grunt build
+```
